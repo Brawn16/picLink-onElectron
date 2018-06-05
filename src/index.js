01da73ea3384ed2b005c4098b67f2b5f93a1a499
Radu <ssd@Radus-MacBook-Pro.local>
2018-06-05 11:00:52 +0100

    package for mac is working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Application from "./Application";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
-registerServiceWorker();
